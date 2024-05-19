5cb9e5f1ab58dd06adf1142bd4d418d75ac4c7f9
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-05-18 19:54:11 -0700

    Require secretWord to not start with p and notify user

--- app.js
+++ app.js
@@ -45,7 +45,13 @@ app.get("/secretWord", (req, res) => {
   res.render("secretWord", { secretWord: req.session.secretWord });
 });
 app.post("/secretWord", (req, res) => {
-  req.session.secretWord = req.body.secretWord;
+  if (req.body.secretWord.toUpperCase()[0] == "P") {
+    req.flash("error", "That word won't work!");
+    req.flash("error", "You can't use words that start with p.");
+  } else {
+    req.session.secretWord = req.body.secretWord;
+    req.flash("info", "The secret word was changed.");
+  }
   res.redirect("/secretWord");
 });
 
