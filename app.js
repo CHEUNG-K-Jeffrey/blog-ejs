5617a7fd8f78e2ce43b086d2dac5b7eefb72e64c
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-06-10 22:54:04 -0700

    Format code

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const helmet = require("helmet")
-const xss = require('xss-clean')
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const express = require("express");
 require("express-async-errors");
 
 const app = express();
 
-const rateLimit = require('express-rate-limit').rateLimit
+const rateLimit = require("express-rate-limit").rateLimit;
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 
 // Apply the rate limiting middleware to all requests.
-app.use(limiter)
+app.use(limiter);
 
 app.use(helmet());
-app.use(xss())
+app.use(xss());
 
 require("dotenv").config(); // to load the .env file into the process.env object
 const session = require("express-session");
@@ -94,7 +94,6 @@ app.use("/secretWord", secretWordRouter);
 const jobs = require("./routes/jobs");
 app.use("/jobs", auth, jobs);
 
-
 app.use((req, res) => {
   res.status(404).send(`That page (${req.url}) was not found.`);
 });
@@ -108,13 +107,13 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    let mongoURL = process.env.MONGO_URI
+    let mongoURL = process.env.MONGO_URI;
     if (process.env.NODE_ENV == "test") {
-      mongoURL = process.env.MONGO_URI_TEST
+      mongoURL = process.env.MONGO_URI_TEST;
     }
-await require("./db/connect")(mongoURL);
+    await require("./db/connect")(mongoURL);
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`),
+      console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
     console.log(error);
