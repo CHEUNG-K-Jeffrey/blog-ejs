75414357c66ecf1c1ce1eba73a3b9eebc26a7bfa
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-06-26 07:38:07 -0700

    Refactor code

--- app.js
+++ app.js
@@ -7,16 +7,14 @@ const app = express();
 
 const rateLimit = require("express-rate-limit").rateLimit;
 
-const limiter = rateLimit({
+// Apply the rate limiting middleware to all requests.
+app.use(rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   // store: ... , // Redis, Memcached, etc. See below.
-});
-
-// Apply the rate limiting middleware to all requests.
-app.use(limiter);
+}));
 
 app.use(helmet());
 app.use(xss());
